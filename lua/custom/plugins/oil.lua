d3bbb201b83a461aa459bde6d349e58bcdb4bd3c
Balázs Góczán <balazs.goczan@gmail.com>
2024-12-13 23:25:06 +0100

    Show hidden files

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -5,6 +5,9 @@ return {
       require('oil').setup {
         columns = { 'icon' },
         keymaps = { ['<C-h>'] = false },
+        view_options = {
+          show_hidden = true,
+        },
       }
 
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
